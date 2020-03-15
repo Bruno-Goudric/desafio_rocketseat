4e53f5d7d16bf0a6bece14c92fdbab90fce8bfb5
Bruno-Goudric <brunolfsports@hotmail.com>
2020-03-15 18:52:43 -0300

    Start Admin Page

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const routes = express.Router()
 const HomeController = require('./app/controllers/HomeController') 
+const recipes = require('./app/controllers/recipe')
 
 routes.get('/', function(req, res) {
     return res.redirect("/home")
 })
 
+// HOME
 routes.get('/home', HomeController.index)
 routes.get('/home/about', HomeController.about)
 routes.get('/home/recipes', HomeController.recipes)
 routes.get('/recipe/:index', HomeController.recipe)
-// routes.get('/instructors/create', instructors.create)
-// routes.get('/instructors/:id', instructors.show)
-// routes.get('/instructors/:id/edit', instructors.edit)
-// routes.post("/instructors", instructors.post)
-// routes.put("/instructors", instructors.put)
-// routes.delete("/instructors", instructors.delete)
 
+// ADMIN
+routes.get("/admin/recipes", recipes.index)
 
 
 
